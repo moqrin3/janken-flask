b80d1f8e0cfc6f58adee85ca1e8bdc4641dd20e2
moqrin3 <moqrin3@gmail.com>
2019-11-17 05:13:57 +0900

    1st commit

---
+++ janken/janken.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, jsonify
+import random
+import json
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def janken():
+    match = {}
+    if request.method == 'POST':
+        client_hand = request.form['hand']
+        match["client_hand"] = client_hand
+        janken_result = pc_choice(client_hand, match)
+
+        return render_template('result.html', client_hand=match["client_hand"],
+                               pc_hand=match["pc_hand"], janken_result=match["result"])
+
+    return render_template('index.html')
+
+
+def pc_choice(client_hand, match):
+    client_hand = client_hand
+    pc_choices = ["グー", "チョキ", "パー"]
+    pc_hand = random.choice(pc_choices)
+    match["pc_hand"] = pc_hand
+
+    draw = '引き分けでした。'
+    win = 'あなたの勝ちです！'
+    lose = 'あなたの負けです...'
+
+    if client_hand == pc_hand:
+        match["result"] = draw
+        return match
+    else:
+        if client_hand == "グー":
+            if pc_hand == "チョキ":
+                match["result"] = win
+                return match
+            else:
+                match["result"] = lose
+                return match
+
+        elif client_hand == "チョキ":
+            if pc_hand == "パー":
+                match["result"] = win
+                return match
+            else:
+                match["result"] = lose
+                return match
+
+        else:
+            if pc_hand == "グー":
+                match["result"] = win
+                return match
+            else:
+                match["result"] = lose
+                return match
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
